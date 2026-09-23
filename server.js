65587cbb33f9504ad59fca883b8fd0a91a235da5
eurasiaaiplatform-ui <eurasiaai.platform@gmail.com>
2026-09-23 23:42:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const server = http.createServer(app);
